1e670cb2976c262e4ed9605389b18e6a466ba6ee
DevAbas <abas.memmedov.94@gmail.com>
2018-11-05 00:45:55 +0400

    Endpoints created for book schema

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const config = require('./config/config').get(process.env.NODE_ENV);
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DATABASE, { useNewUrlParser: true });
+mongoose.connect(config.DATABASE, { useNewUrlParser: true, useCreateIndex: true, });
 
 // Load models
 const { User } = require('./models/user');
@@ -14,6 +14,63 @@ const { Book } = require('./models/book');
 
 app.use(bodyParse.json());
 app.use(cookieParser());
+
+// GET //
+app.get('/api/book', (req, res) => {
+  let id = req.query.id;
+
+  Book.findById(id, (err, doc) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(doc)
+  })
+})
+
+app.get('/api/books', (req, res) => {
+  let skip = parseInt(req.query.skip);
+  let limit = parseInt(req.query.limit);
+  let order = req.query.order;
+
+  Book.find().skip(skip).sort({_id: order}).limit(limit).exec((err, doc) => {
+    if(err) return res.status(400).send(err);
+
+    res.send(doc)
+  })
+})
+
+// POST //
+app.post('/api/book', (req, res) => {
+  const book = new Book(req.body);
+
+  book.save((err, doc) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).json({
+      post: true,
+      bookId: doc._id
+    })
+  })
+})
+
+// UPDATE //
+app.post('/api/updateBook', (req, res) => {
+  Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
+    if(err) return res.status(400).send(err)
+    res.json({
+      success: true,
+      doc
+    })
+  })
+})
+
+
+// DELETE //
+app.delete('/api/deleteBook', (req, res) => {
+  let id = req.query.id;
+
+  Book.findByIdAndRemove(id, (err, doc) => {
+    if(err) return res.status(400).send(err)
+    res.json(true)
+  })
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
