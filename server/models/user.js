23c48e96e82e4fda61c87247de8df3267b2dccec
DevAbas <abas.memmedov.94@gmail.com>
2018-11-05 18:13:01 +0400

    User login and register enpoints created

--- server/models/user.js
+++ server/models/user.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config').get(process.env.NODE_ENV);
 const Schema = mongoose.Schema;
+const SALT_I = 10;
 
 const userSchema = new Schema({
   email:{
     type: String,
-    unique: true,
+    unique: 1,
     trim: true,
     required: true
   },
@@ -32,6 +33,42 @@ const userSchema = new Schema({
     type: String
   }
 });
+
+userSchema.pre('save', function(next){
+  var user = this;
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(SALT_I, function(err, salt){
+      if(err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if(err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(),config.SECRET);
+
+  user.token = token;
+  user.save(function(err, user){
+    if(err) return cb(err);
+    cb(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
